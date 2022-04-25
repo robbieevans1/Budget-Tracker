67f857854dec2a11dd7c7ddf81f72dd07c3c38d3
robcabobb <robcabobb@gmail.com>
2022-04-25 04:54:42 -0400

    added install event listener

--- public/service-worker.js
+++ public/service-worker.js
@@ -29,3 +29,13 @@ self.addEventListener("fetch", function (e) {
 		})
 	);
 });
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+        return cache.addAll(FILES_TO_CACHE)
+    })
+  )
+})
+
