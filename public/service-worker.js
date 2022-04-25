3e4fc21ec7e6baf42c99f427f6f2837e070deb08
robcabobb <robcabobb@gmail.com>
2022-04-25 05:03:12 -0400

    added activate event listener to delete outdated caches. finished with service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -15,6 +15,7 @@ const FILES_TO_CACHE = [
 	"./icons/icon-512x512.png",
 ];
 
+// fetch
 self.addEventListener("fetch", function (e) {
 	console.log("fetch request : " + e.request.url);
 	e.respondWith(
@@ -30,12 +31,36 @@ self.addEventListener("fetch", function (e) {
 	);
 });
 
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log('installing cache : ' + CACHE_NAME)
-        return cache.addAll(FILES_TO_CACHE)
-    })
-  )
-})
+// install
+self.addEventListener("install", function (e) {
+	e.waitUntil(
+		caches.open(CACHE_NAME).then(function (cache) {
+			console.log("installing cache : " + CACHE_NAME);
+			return cache.addAll(FILES_TO_CACHE);
+		})
+	);
+});
 
+// Delete outdated caches
+self.addEventListener("activate", function (e) {
+	e.waitUntil(
+		caches.keys().then(function (keyList) {
+			// `keyList` contains all cache names under your username.github.io
+			// filter out ones that has this app prefix to create keeplist
+      let cacheKeeplist = keyList.filter(function(key) {
+        return key.indexOf(APP_PREFIX)
+      })
+      // add current cache name to keeplist
+      cacheKeeplist.push(CACHE_NAME)
+
+      return Promise.all(
+        keyList.map(function(key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log('deleting cache : ' + keyList[i])
+            return caches.delete(keyList[i])
+          }
+        })
+      )
+		})
+	);
+});
